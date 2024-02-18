629a7d9955446f8593090064435ea428fbf19594
heitzuli <heidi.poels@gmail.com>
2024-02-18 14:44:48 +0200

    Add POST

--- src/main/java/com/heitzuli/socialbackend/controllers/PostController.java
+++ src/main/java/com/heitzuli/socialbackend/controllers/PostController.java
@@ -1,8 +1,12 @@
 package com.heitzuli.socialbackend.controllers;
 
 import com.heitzuli.socialbackend.dto.Post;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +17,7 @@ import java.util.stream.Stream;
 @CrossOrigin
 @RequestMapping("/posts")
 public class PostController {
+    Logger logger = LoggerFactory.getLogger(PostController.class);
     @GetMapping
     public List<Post> findAll() {
         var post1 = new Post(1, "Hello wrold");
@@ -20,4 +25,11 @@ public class PostController {
         var post3 = new Post(3, "Hello WORLD");
         return Stream.of(post1, post2, post3).toList();
     }
+
+    @PostMapping
+    public void addPost(@RequestBody Post post) {
+        logger.info(post.text());
+    }
 }
+
+
